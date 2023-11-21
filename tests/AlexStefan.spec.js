050571bef5ce9016fb57e3ffeb1ebaaf6be007a3
AlexStefan <stefan.alex170299@gmail.com>
2023-11-21 23:15:03 +0200

    added AlexStefan.spec.js

---
+++ tests/AlexStefan.spec.js
@@ -0,0 +1,9 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test("has title", async ({ page }) => {
+  await page.goto("https://www.ajokeaday.com/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Joke of the Day/);
+});
