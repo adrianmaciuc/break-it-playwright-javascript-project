6d60eb1a2fa4739e0537f2ebe0e41e8c136c6f40
Popescu_Dan <popedan99@yahoo.com>
2023-11-24 13:32:08 +0200

    Retest1

---
+++ tests/PopescuDan.spec.js
@@ -0,0 +1,9 @@
+// @ts-check - Danut
+const { test, expect } = require("@playwright/test");
+
+test("has title", async ({ page }) => {
+  await page.goto("https://manhuaplus.com");
+
+  // Expect a title "to contain" a substring !!!
+  await expect(page).toHaveTitle(/ManhuaPlus/);
+});
