bc6b06d26901d1ed5340be462ad2405bdf33ac45
Alex <61619827+AlexStefan17@users.noreply.github.com>
2023-11-24 15:18:19 +0200

    Update HotSellersSection.spec.js first scenario

--- tests/homePage/HotSellersSection.spec.js
+++ tests/homePage/HotSellersSection.spec.js
@@ -1,19 +1,8 @@
 // @ts-check
 const { test, expect } = require("@playwright/test");
 
-test("has title", async ({ page }) => {
+test("Six items displayed at hot sellers", async ({ page }) => {
   await page.goto("https://magento.softwaretestingboard.com/");
-
-  const hotSellers = [
-    "Radiant Tee",
-    "Breathe-Easy Tank",
-    "Argus All-Weather Tank",
-    "Hero Hoodie",
-    "Fusion Backpack",
-    "Push It Messenger Bag",
-  ];
-
-  for (const item of hotSellers) {
-    await expect(page.getByRole("link", { name: item }).first()).toBeVisible();
-  }
+  const productItems = (await page.$$('//li[@class="product-item"]')).length;
+  expect(productItems).toBe(6);
 });
