1aee8538e36256f2a9e8e0d716d575a0a6557710
Alex <61619827+AlexStefan17@users.noreply.github.com>
2023-11-24 17:26:07 +0200

    added second scenario

--- tests/AlexStefan.spec.js
+++ tests/AlexStefan.spec.js
@@ -7,3 +7,32 @@ test("has title", async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Joke of the Day/);
 });
+
+
+test("Item redirects correctly on home page", async ({ page }) => {
+  await page.goto("https://magento.softwaretestingboard.com/");
+
+  const productItems = await page.$$('//li[@class="product-item"]');
+  const priceItems = await page.locator('.price').allTextContents();
+  const nameItems = await page.locator(".product-item-link").allInnerTexts();
+
+  for (let i = 0; i < productItems.length; i++) {
+    // Re-fetch the elements inside the loop to ensure they are still attached to the DOM
+    const updatedProductItems = await page.$$('//li[@class="product-item"]');
+    
+    // Go to item`s page
+    await updatedProductItems[i].click();
+
+    // get the name of the item
+    const nameItemsPage = await page.locator("//span[@class='base']").innerText();
+
+    // get the price of the item
+    const priceItemPage = await page.locator('.price-wrapper').first().innerText();
+
+    expect(nameItemsPage).toBe(nameItems[i]);
+    expect(priceItemPage).toBe(priceItems[i]);
+    
+    await page.goto("https://magento.softwaretestingboard.com/");
+    
+  }
+});
