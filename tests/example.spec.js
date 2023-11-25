9ffd12c9d9c031546aa9fac0a21a408d128444c9
Radu Moldovan <radumoldovan08@yahoo.com>
2023-11-25 08:42:03 +0200

    Updated host seller section

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -17,3 +17,4 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
