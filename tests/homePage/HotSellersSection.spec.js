ff09f0698ba88ee13aad5b4b175f98b93b98c54b
AlexStefan <stefan.alex170299@gmail.com>
2023-11-25 18:59:01 +0200

    applied  adrianmaciuc suggestions

--- tests/homePage/HotSellersSection.spec.js
+++ tests/homePage/HotSellersSection.spec.js
@@ -3,37 +3,32 @@ const { test, expect } = require("@playwright/test");
 
 test("Six items displayed at hot sellers", async ({ page }) => {
   await page.goto("https://magento.softwaretestingboard.com/");
-  const productItems = (await page.$$('//li[@class="product-item"]')).length;
-  expect(productItems).toBe(6);
+  const productItems = await page.$$(".product-item");
+  expect(productItems).toHaveLength(6);
 });
 
 test("Item redirects correctly on home page", async ({ page }) => {
   await page.goto("https://magento.softwaretestingboard.com/");
 
-  const productItems = await page.$$('//li[@class="product-item"]');
+  const productItems = await page.$$(".product-item");
   const priceItems = await page.locator(".price").allTextContents();
   const nameItems = await page.locator(".product-item-link").allInnerTexts();
 
-  for (let i = 0; i < productItems.length; i++) {
+  for (let item in productItems) {
     // Re-fetch the elements inside the loop to ensure they are still attached to the DOM
-    const updatedProductItems = await page.$$('//li[@class="product-item"]');
+    const updatedProductItems = await page.$$(".product-item");
 
     // Go to item`s page
-    await updatedProductItems[i].click();
+    await updatedProductItems[item].click();
 
     // get the name of the item
-    const nameItemsPage = await page
-      .locator("//span[@class='base']")
-      .innerText();
+    const nameItemsPage = await page.locator("span.base").innerText();
 
     // get the price of the item
-    const priceItemPage = await page
-      .locator(".price-wrapper")
-      .first()
-      .innerText();
+    const priceItemPage = await page.locator(".product-info-main .price");
 
-    expect(nameItemsPage).toBe(nameItems[i]);
-    expect(priceItemPage).toBe(priceItems[i]);
+    expect(nameItemsPage).toContain(nameItems[item]);
+    expect(priceItemPage).toHaveText(priceItems[item]);
 
     await page.goto("https://magento.softwaretestingboard.com/");
   }
