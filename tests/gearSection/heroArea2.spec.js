63e9fec3a1b7d462531171f468ba47ad3b03df00
AlexStefan <stefan.alex170299@gmail.com>
2023-11-26 22:02:18 +0200

    added tests for  Gear Section > Hero Area 2 #77

---
+++ tests/gearSection/heroArea2.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+
+test("Gear section hero images-1", async ({ page }) => {
+  await page.goto("https://magento.softwaretestingboard.com/gear.html");
+  await page.locator(".block-promo.gear-category-equipment").click();
+  await expect(page.url()).toContain("/gear/fitness-equipment.htm");
+});
+
+test("Gear section hero images-2", async ({ page }) => {
+  await page.goto("https://magento.softwaretestingboard.com/gear.html");
+  await page.locator(".block-promo.gear-category-watches").click();
+  await expect(page.url()).toContain("watches");
+});
