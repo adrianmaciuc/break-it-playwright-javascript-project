c44858681b572a7531e668f14edc27763f5a15b4
Iacob Claudiu <68840483+iclaudiu98@users.noreply.github.com>
2023-11-27 21:45:09 +0200

    Update ClaudiuIacob.spec.js

--- tests/ClaudiuIacob.spec.js
+++ tests/ClaudiuIacob.spec.js
@@ -4,7 +4,6 @@ test('Verify if only four lumas items are listed on whats new page', async ({ pa
   await page.goto('https://magento.softwaretestingboard.com/what-is-new.html');
   await expect(page).toHaveURL(/what-is-new/);
   await page.waitForSelector('.product-image-photo');
-
   const productImageElements = await page.$$('.product-image-photo');
   expect(productImageElements.length).toBe(4);
   console.log('Avem 4 produse lumas');
@@ -13,7 +12,6 @@ test('Verify if only four lumas items are listed on whats new page', async ({ pa
 test('Verify if I am on the what is new page and I click on the Stellar solar jacket from latest lumas I am redirect to page../stellar-solar-jacket.html.', async ({ page }) => {
   await page.goto('https://magento.softwaretestingboard.com/what-is-new.html');
   await expect(page).toHaveURL(/what-is-new/);
-
   await page.click('text=Stellar Solar Jacket');
   await expect(page).toHaveURL('https://magento.softwaretestingboard.com/stellar-solar-jacket.html');
   console.log('Suntem pe pagina stellar-solar-jacket.html');
