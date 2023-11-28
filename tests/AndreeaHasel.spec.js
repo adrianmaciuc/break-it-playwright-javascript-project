7dd9532f486efb9180efcf6a7ebd9174c024547a
Andreea Hasel <haselandreea@yahoo.com>
2023-11-27 23:15:05 -0700

    add test for has title

---
+++ tests/AndreeaHasel.spec.js
@@ -0,0 +1,9 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test("has title", async ({ page }) => {
+  await page.goto("https://www.wikipedia.org/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Wikipedia/);
+});
