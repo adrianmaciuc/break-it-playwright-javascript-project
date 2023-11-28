c3f07187c53d790850e665e6c13d7e06145de9da
Claudiu Iacob <iacobclaudiu1998@gmail.com>
2023-11-28 23:18:10 +0200

    optimization

--- tests/ClaudiuIacob.spec.js
+++ tests/ClaudiuIacob.spec.js
@@ -3,8 +3,8 @@ const { test, expect } = require('@playwright/test');
 test('Verify if only four lumas items are listed on whats new page', async ({ page }) => {
   await page.goto('https://magento.softwaretestingboard.com/what-is-new.html');
   await expect(page).toHaveURL(/what-is-new/);
-  const productImageElements = await page.$$('.product-image-photo');
-  expect(productImageElements.length).toBe(4);
+  const products = page.locator(".product-image-photo");
+  await expect(products).toHaveCount(4);
 });
 
 test('Verify if I am on the what is new page and I click on the Stellar solar jacket from latest lumas I am redirect to page../stellar-solar-jacket.html.', async ({ page }) => {
