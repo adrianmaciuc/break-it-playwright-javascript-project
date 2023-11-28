6af1a9fcc44f8bce86dba6297c0ea97cded0d70b
Andreea Hasel <haselandreea@yahoo.com>
2023-11-27 23:20:53 -0700

    comments deleted

--- tests/AndreeaHasel.spec.js
+++ tests/AndreeaHasel.spec.js
@@ -1,9 +1,7 @@
-// @ts-check
 const { test, expect } = require("@playwright/test");
 
 test("has title", async ({ page }) => {
   await page.goto("https://www.wikipedia.org/");
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Wikipedia/);
 });
