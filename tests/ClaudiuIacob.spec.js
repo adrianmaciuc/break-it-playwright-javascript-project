a6cecabf0393e1f5427c7653763c89a2bdb67c31
Claudiu Iacob <iacobclaudiu1998@gmail.com>
2023-11-29 21:11:56 +0200

    remove asserts that are not mandatory

--- tests/ClaudiuIacob.spec.js
+++ tests/ClaudiuIacob.spec.js
@@ -2,14 +2,12 @@ const { test, expect } = require('@playwright/test');
 
 test('Verify if only four lumas items are listed on whats new page', async ({ page }) => {
   await page.goto('https://magento.softwaretestingboard.com/what-is-new.html');
-  await expect(page).toHaveURL(/what-is-new/);
   const products = page.locator(".product-image-photo");
   await expect(products).toHaveCount(4);
 });
 
 test('Verify if by Clicking on  Stellar Solar Jacket the page is redirect to the clicked product', async ({ page }) => {
   await page.goto('https://magento.softwaretestingboard.com/what-is-new.html');
-  await expect(page).toHaveURL(/what-is-new/);
   await page.getByText('Stellar Solar Jacket').click()
   await expect(page).toHaveURL('https://magento.softwaretestingboard.com/stellar-solar-jacket.html');
 });
