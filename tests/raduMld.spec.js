d457dc0ca2385ae9c56d41980076b45de04edb6a
Radu Moldovan <radumoldovan08@yahoo.com>
2023-12-05 16:23:10 +0200

    removed console.log

--- tests/raduMld.spec.js
+++ tests/raduMld.spec.js
@@ -10,7 +10,6 @@ test("Check if the user is on the Home Page", async ({ page }) => {
 test("Count products on the home page", async ({ page }) => {
   await page.goto("https://magento.softwaretestingboard.com/");
   const numberOfProducts = await page.locator(".product-item").count();
-  console.log("Number of products on the home page is:", numberOfProducts);
   expect(numberOfProducts).toBe(6);
 });
 
