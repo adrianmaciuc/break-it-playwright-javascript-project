1cf63a0a02620ae27a8cb9428756038845c566b5
Radu Moldovan <radumoldovan08@yahoo.com>
2023-12-05 08:12:33 +0200

    removed evalALL and $$ methods

--- tests/raduMld.spec.js
+++ tests/raduMld.spec.js
@@ -1,29 +1,39 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-
-test('Check if the user is on the Home Page', async ({ page }) => {
-    await page.goto('https://magento.softwaretestingboard.com/');
-    await expect(page).toHaveURL('https://magento.softwaretestingboard.com/');
-    await expect(page).toHaveTitle("Home Page"); 
+test("Check if the user is on the Home Page", async ({ page }) => {
+  await page.goto("https://magento.softwaretestingboard.com/");
+  await expect(page).toHaveURL("https://magento.softwaretestingboard.com/");
+  await expect(page).toHaveTitle("Home Page");
 });
 
-
-test('Count products on the home page', async ({ page }) => {
-
-    await page.goto('https://magento.softwaretestingboard.com/');
-    const numberOfProducts = await page.locator('.product-item').evaluateAll(products => products.length);
-    console.log('Number of products on the home page is:', numberOfProducts);
-    expect(numberOfProducts).toBe(6)
+test("Count products on the home page", async ({ page }) => {
+  await page.goto("https://magento.softwaretestingboard.com/");
+  const numberOfProducts = await page
+    .locator(".product-item")
+    .evaluateAll((products) => products.length);
+  console.log("Number of products on the home page is:", numberOfProducts);
+  expect(numberOfProducts).toBe(6);
 });
 
-test('Select random products from home page', async ({ page }) => {
-    await page.goto('https://magento.softwaretestingboard.com/')
-    const numberOfProducts = await page.$$('.product-item');
-    const randomProduct = numberOfProducts[Math.floor(Math.random() * numberOfProducts.length)];
-    const randomTitleBeforeClick = await randomProduct.$eval('.product-item-link', element => element.textContent.trim());
-    await randomProduct.click();
-    const randomTitleAfterRedirect = await page.$eval('span[data-ui-id="page-title-wrapper"][itemprop="name"]', element => element.textContent.trim());
-    expect(randomTitleAfterRedirect).toEqual(randomTitleBeforeClick);
-    console.log('The title name is the same:', randomTitleAfterRedirect === randomTitleBeforeClick);
-  });
+test("Select random products from home page", async ({ page }) => {
+  await page.goto("https://magento.softwaretestingboard.com/");
+  const numberOfProducts = await page.$$(".product-item");
+  const randomProduct =
+  numberOfProducts[Math.floor(Math.random() * numberOfProducts.length)];
+  const pageTitles = await page.locator(".product-item-link").allInnerTexts();
+  await randomProduct.click();
+
+  const pageTitle = await page.locator(".product-info-main .base").innerText();
+  let matchFound = false;
+  for (let i = 0; i < pageTitles.length; i++) {
+    if (pageTitle === pageTitles[i]) {
+      console.log(`The home title name ${pageTitles[i]} is the same as product title name ${pageTitle}`);
+      matchFound = true;
+      break;
+    }
+  }
+  if (!matchFound) {
+  console.log(`The home title name is not the same as product title name ${pageTitle}`);
+  }
+});
