51c41b3feca1a771af0990bf548ab95562cce29f
Radu Moldovan <radumoldovan08@yahoo.com>
2023-12-05 09:55:00 +0200

    removed evaluateALL and $$

--- tests/raduMld.spec.js
+++ tests/raduMld.spec.js
@@ -9,18 +9,16 @@ test("Check if the user is on the Home Page", async ({ page }) => {
 
 test("Count products on the home page", async ({ page }) => {
   await page.goto("https://magento.softwaretestingboard.com/");
-  const numberOfProducts = await page
-    .locator(".product-item")
-    .evaluateAll((products) => products.length);
+  const numberOfProducts = await page.locator(".product-item").count();
   console.log("Number of products on the home page is:", numberOfProducts);
   expect(numberOfProducts).toBe(6);
 });
 
+
 test("Select random products from home page", async ({ page }) => {
   await page.goto("https://magento.softwaretestingboard.com/");
-  const numberOfProducts = await page.$$(".product-item");
-  const randomProduct =
-  numberOfProducts[Math.floor(Math.random() * numberOfProducts.length)];
+  const numberOfProducts = await page.locator(".product-item").all();
+  const randomProduct = numberOfProducts[Math.floor(Math.random() * numberOfProducts.length)];
   const pageTitles = await page.locator(".product-item-link").allInnerTexts();
   await randomProduct.click();
 
@@ -34,6 +32,6 @@ test("Select random products from home page", async ({ page }) => {
     }
   }
   if (!matchFound) {
-  console.log(`The home title name is not the same as product title name ${pageTitle}`);
+  console.log(`The home title name is not the same as product title name`);
   }
 });
