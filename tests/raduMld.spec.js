eb00153a24ecedea0a27b6026939783f51b6c36e
Radu Moldovan <radumoldovan08@yahoo.com>
2023-12-07 20:55:06 +0200

    added await for expect

--- tests/raduMld.spec.js
+++ tests/raduMld.spec.js
@@ -10,7 +10,7 @@ test("Check if the user is on the Home Page", async ({ page }) => {
 test("Count products on the home page", async ({ page }) => {
   await page.goto("https://magento.softwaretestingboard.com/");
   const numberOfProducts = await page.locator(".product-item").count();
-  expect(numberOfProducts).toBe(6);
+  await expect(numberOfProducts).toBe(6);
 });
 
 
@@ -21,5 +21,5 @@ test("Select random products from home page", async ({ page }) => {
   const pageTitles = await page.locator(".product-item-link").allInnerTexts();
   await randomProduct.click();
   const pageTitle = await page.locator(".product-info-main .base").innerText();
-  expect(pageTitles).toContain(pageTitle);
+  await expect(pageTitles).toContain(pageTitle);
 });
