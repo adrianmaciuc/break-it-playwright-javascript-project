8d5aaeb86de1719c5d4bd6e9bd9be04f003e23bb
Mirel P <mirel.palii@gmail.com>
2023-12-12 08:49:53 +0200

    merge one e2e cart

--- tests/cart/addItemToCart.spec.js
+++ tests/cart/addItemToCart.spec.js
@@ -3,6 +3,7 @@ const HomePageUrl = 'https://magento.softwaretestingboard.com/';
 
 test('Navigate to top tees', async ({ page }) => {
   await page.goto(HomePageUrl);
+  await page.waitForTimeout(1000);
   await page.locator('#ui-id-5').hover();
   await page.locator('#ui-id-17').hover();
   await page.locator('#ui-id-21').click();
@@ -10,12 +11,7 @@ test('Navigate to top tees', async ({ page }) => {
   await expect(page.url()).toEqual(
     'https://magento.softwaretestingboard.com/men/tops-men/tees-men.html'
   );
-});
 
-test('Filter top tees by color green and select element', async ({ page }) => {
-  await page.goto(
-    'https://magento.softwaretestingboard.com/men/tops-men/tees-men.html'
-  );
   await page.getByRole('tab', { name: 'Color' }).click();
   await page.locator('#narrow-by-list [option-id="53"]').click();
   await expect(page.locator('.product-item')).toHaveCount(4);
@@ -26,16 +22,14 @@ test('Filter top tees by color green and select element', async ({ page }) => {
   await expect(page.url()).toEqual(
     'https://magento.softwaretestingboard.com/atomic-endurance-running-tee-v-neck.html'
   );
-});
 
-test('Add item to cart', async ({ page }) => {
-  await page.goto(
-    'https://magento.softwaretestingboard.com/atomic-endurance-running-tee-v-neck.html'
-  );
-  await page.getByLabel('XL').click();
-  await page.getByLabel('Green').click();
-  await page.locator('#qty').fill('3');
+  const selectOptionsForProduct = async (size, color, quantity) => {
+    await page.getByLabel(size).click();
+    await page.getByLabel(color).click();
+    await page.locator('#qty').fill(quantity);
+  };
 
+  await selectOptionsForProduct('XL', 'Green', '3');
   await page.getByTitle('Add to Cart').click();
   const response = await page.waitForResponse('**/customer/section/load/*');
   const respBody = await response.json();
