8e48a48ad71007ab497881e3af45bb7ecb3cfe31
Adrian Maciuc <adrian.maciuc@gmail.com>
2023-12-17 14:16:10 +0200

    update tests with latest config changes

--- tests/homePage/homePageHeader2.spec.js
+++ tests/homePage/homePageHeader2.spec.js
@@ -1,59 +1,55 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-const HomePageUrl = 'https://magento.softwaretestingboard.com/';
+const HomePageUrl = "https://magento.softwaretestingboard.com/";
 
-test('Search suggestion in header', async ({ page }) => {
-  const inputSelector = '#search';
-
+test("Search suggestion in header", async ({ page }) => {
   await page.goto(HomePageUrl);
-  await expect(page).toHaveTitle('Home Page');
+  await expect(page).toHaveTitle("Home Page");
 
-  await page
-    .locator(inputSelector)
-    .pressSequentially('jackets', { delay: 100 });
+  await page.getByTestId("search").pressSequentially("jackets", { delay: 100 });
   await page.waitForTimeout(1000);
 
-  const parentLocator = page.locator('#search_autocomplete');
+  const parentLocator = page.getByTestId("search_autocomplete");
   const childLocator = parentLocator.locator('[role="option"]');
 
   const lenghtEl = await childLocator.count();
   await expect(lenghtEl).toEqual(8);
 
   for (let i = 0; i < lenghtEl; i++) {
-    await expect(childLocator.nth(i)).toContainText('jacket', {
+    await expect(childLocator.nth(i)).toContainText("jacket", {
       ignoreCase: true,
     });
   }
 });
 
-test('Logo click redirect home page', async ({ page }) => {
+test("Logo click redirect home page", async ({ page }) => {
   await page.goto(HomePageUrl);
-  await page.getByLabel('store logo').click();
+  await page.getByLabel("store logo").click();
   expect(page.url()).toEqual(HomePageUrl);
 });
 
-test('Click on cart is empty', async ({ page }) => {
+test("Click on cart is empty", async ({ page }) => {
   await page.goto(HomePageUrl);
 
   await page.waitForTimeout(1000);
 
   await page.locator('[data-block="minicart"] .action.showcart').click();
-  await expect(page.locator('#minicart-content-wrapper')).toContainText(
-    'You have no items in your shopping cart.'
+  await expect(page.getByTestId("minicart-content-wrapper")).toContainText(
+    "You have no items in your shopping cart."
   );
 });
 
-test('Search functionality with result', async ({ page }) => {
+test("Search functionality with result", async ({ page }) => {
   await page.goto(HomePageUrl);
-  await page.locator('#search').fill('jackets');
+  await page.getByTestId("search").fill("jackets");
 
-  await page.keyboard.press('Enter');
-  const listItem = await page.locator('.product-item');
+  await page.keyboard.press("Enter");
+  const listItem = await page.locator(".product-item");
   await expect(listItem).toHaveCount(1);
 
   const cardText = (
     await page.locator('[class="product name product-item-name"]>a').innerText()
   ).trim();
 
-  await expect(cardText).toEqual('Jade Yoga Jacket');
+  await expect(cardText).toEqual("Jade Yoga Jacket");
 });
--- tests/initialSetupTests/Iulian.spec.js
+++ tests/initialSetupTests/Iulian.spec.js
@@ -1,11 +1,12 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('coolors.co has the correct title', async ({ page }) => {
+test("coolors.co has the correct title", async ({ page }) => {
   // Click the get started link.
-  await page.goto('https://coolors.co/');
+  await page.goto("https://coolors.co/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('Coolors - The super fast color palettes generator!');
+  await expect(page).toHaveTitle(
+    "Coolors - The super fast color palettes generator!"
+  );
 });
-
