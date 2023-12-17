720d001f2144a7fe1c68112bd618a13a882e41bb
Adrian Maciuc <adrian.maciuc@gmail.com>
2023-12-17 14:09:48 +0200

    set id as idAttribute

--- playwright.config.js
+++ playwright.config.js
@@ -23,6 +23,7 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+    testIdAttribute: "id",
   },
 
   /* Configure projects for major browsers */
